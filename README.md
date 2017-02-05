4e5aba0e242befd00b25d2d813c50a12bf5ee725
Tristan971 <tristandeloche@gmail.com>
2017-02-05 21:02:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lyrebird
+
+(to come)
---
+++ src/main/java/moe/lyrebird/LyrebirdApplication.java
@@ -0,0 +1,12 @@
+package moe.lyrebird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyrebirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyrebirdApplication.class, args);
+	}
+}
