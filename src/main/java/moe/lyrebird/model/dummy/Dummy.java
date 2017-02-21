67f691e68849f8e5f63b60bb2556ee3c4bfba25a
Tristan971 <tristandeloche@gmail.com>
2017-02-21 02:21:59 +0100

    Add a dumy generic class for simple stubs. Doesn't work with collections

---
+++ src/main/java/moe/lyrebird/model/dummy/Dummy.java
@@ -0,0 +1,25 @@
+package moe.lyrebird.model.dummy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This class helps with stubs or other types of elements that
+ * require non-null casted elements temporarily.
+ * Use with caution.
+ */
+@Slf4j
+public class Dummy {
+    /**
+     * Returns a dummy object of some class
+     *
+     * @param tClass
+     *         The class queried
+     * @param <T>
+     *         The object
+     * @return A false object of class T. Often will break with new APIs.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getDummy(final Class<T> tClass) {
+        return (T) new Object();
+    }
+}
