7ff823e01ecd34b237dd295f97fd12e3743cbf8a
Tristan971 <tristandeloche@gmail.com>
2017-03-30 12:34:49 +0200

    Remove useless overriding of types for SessionRepository

--- src/main/java/moe/lyrebird/model/sessions/SessionRepository.java
+++ src/main/java/moe/lyrebird/model/sessions/SessionRepository.java
@@ -2,61 +2,10 @@ package moe.lyrebird.model.sessions;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 /**
  * This class is the interface of the DAO of the application.
  * It is used to store {@link Session}s with their credentials
  * in the database.
  */
 public interface SessionRepository extends CrudRepository<Session, String> {
-    
-    /**
-     * Saves a session to the repository.
-     *
-     * @param session
-     *         The session to save
-     * @param <S>
-     *         {@link Session} or a subtype of it.
-     * @return The session you just saved.
-     */
-    @Override
-    <S extends Session> S save(final S session);
-    
-    /**
-     * Gets a session from the repo using the UID.
-     *
-     * @param uid
-     *         The session's UID.
-     * @return The session.
-     */
-    @Override
-    Optional<Session> findOne(final String uid);
-    
-    /**
-     * Finds all the saved sessions.
-     *
-     * @return The saved sessions
-     */
-    @Override
-    Iterable<Session> findAll();
-    
-    /**
-     * Checks if a session exists in the repository (from the UID).
-     *
-     * @param uid
-     *         The uid of the session looked for
-     * @return Whether it exists or not.
-     */
-    @Override
-    boolean exists(final String uid);
-    
-    /**
-     * Deletes a session from the repo.
-     *
-     * @param uid
-     *         The ID of the session to be deleted.
-     */
-    @Override
-    void delete(final String uid);
 }
