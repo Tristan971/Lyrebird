4a2825aba6b34bd3251c1ff34b7160932012fb39
Tristan971 <tristandeloche@gmail.com>
2017-03-31 17:04:29 +0200

    Improve ErrorPane

--- src/main/java/moe/lyrebird/view/views/ErrorPane.java
+++ src/main/java/moe/lyrebird/view/views/ErrorPane.java
@@ -1,14 +1,12 @@
 package moe.lyrebird.view.views;
 
-import javafx.application.Platform;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import lombok.experimental.UtilityClass;
+import moe.lyrebird.view.util.FXDOMUtils;
+import moe.lyrebird.view.util.StageUtils;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -20,30 +18,23 @@ import java.util.stream.Collectors;
 public class ErrorPane {
     public static Pane of(final String message, final Throwable throwable) {
         final Label messageLabel = new Label(message);
-        final TextArea throwableDataLabel = new TextArea(
-                "Message : \n" +
-                        throwable.getMessage() +
-                        "\nStackTrace:\n" +
-                        Arrays.stream(throwable.getStackTrace())
-                                .map(StackTraceElement::toString)
-                                .collect(Collectors.joining("\n"))
-        );
-        AnchorPane.setLeftAnchor(messageLabel, 20.0);
-        messageLabel.setStyle("-fx-font-weight: bold !important;");
-        AnchorPane.setTopAnchor(throwableDataLabel, 20.0);
-        AnchorPane.setBottomAnchor(throwableDataLabel, 20.0);
-        AnchorPane.setLeftAnchor(throwableDataLabel, 20.0);
-        AnchorPane.setRightAnchor(throwableDataLabel, 20.0);
+        final TextArea throwableDataLabel = new TextArea(formatErrorMessage(throwable));
     
+        messageLabel.getStyleClass().add("errorPane");
+        FXDOMUtils.centerNode(throwableDataLabel, 20.0);
         return new AnchorPane(messageLabel, throwableDataLabel);
     }
     
+    public static String formatErrorMessage(Throwable t) {
+        return "Message : \n" +
+                t.getMessage() +
+                "\nStackTrace:\n" +
+                Arrays.stream(t.getStackTrace())
+                        .map(StackTraceElement::toString)
+                        .collect(Collectors.joining("\n"));
+    }
+    
     public static void displayErrorPaneOf(final String message, final Throwable throwable) {
-        final Pane errorPane = of(message, throwable);
-        final Scene scene = new Scene(errorPane);
-        final Stage errorStage = new Stage(StageStyle.DECORATED);
-        errorStage.setScene(scene);
-        errorStage.setTitle(message);
-        Platform.runLater(errorStage::show);
+        StageUtils.stageOf(message, of(message, throwable), false);
     }
 }
