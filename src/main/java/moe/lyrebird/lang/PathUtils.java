dd15ed133f28a1caeb6aaf6badf54bd540f9cc9b
Tristan971 <tristandeloche@gmail.com>
2017-03-31 17:02:53 +0200

    Improve PathUtils to stream files in a folder

--- src/main/java/moe/lyrebird/lang/PathUtils.java
+++ src/main/java/moe/lyrebird/lang/PathUtils.java
@@ -1,16 +1,23 @@
 package moe.lyrebird.lang;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * This class is because Brian Goetz doesn't like you reading files from the classpath
  */
+@Slf4j
 @UtilityClass
 public final class PathUtils {
     /**
@@ -34,5 +41,28 @@ public final class PathUtils {
         assert resURI != null :
                 "Maven probably didn't copy contents of src/main/resources into target folder. But idk why.";
         return Paths.get(resURI);
+    }
+    
+    /**
+     * Returns a stream from the files in the given directory.
+     * Simple wrapper around {@link DirectoryStream}.
+     *
+     * @param directory
+     *         The directory to iterate over
+     * @return A stream of the files under the given directory or an empty stream if
+     * the {@link Path} was not a directory.
+     */
+    public static Stream<Path> filesInDirectory(final Path directory) {
+        try {
+            final DirectoryStream<Path> files = Files.newDirectoryStream(directory);
+            return StreamSupport.stream(files.spliterator(), false);
+        } catch (final IOException e) {
+            log.error(
+                    "Could not access files at location {}",
+                    directory.toAbsolutePath().toString(),
+                    e
+            );
+            return Stream.empty();
+        }
     }
 }
