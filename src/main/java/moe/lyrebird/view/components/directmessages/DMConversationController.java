b616ca81d0137b789b5a98804e5d0a7055898849
Tristan971 <tristandeloche@gmail.com>
2018-06-11 22:04:45 +0200

    Very basic fixes for direct messages

--- src/main/java/moe/lyrebird/view/components/directmessages/DMConversationController.java
+++ src/main/java/moe/lyrebird/view/components/directmessages/DMConversationController.java
@@ -1,6 +1,5 @@
 package moe.lyrebird.view.components.directmessages;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -12,15 +11,21 @@ import org.slf4j.LoggerFactory;
 import twitter4j.DirectMessage;
 import twitter4j.User;
 
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.property.ReadOnlyProperty;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
+import java.util.Collections;
 import java.util.function.Supplier;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
 @Component
-@Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(scopeName = SCOPE_PROTOTYPE)
 public class DMConversationController implements FxmlController {
 
     private static final Logger LOG = LoggerFactory.getLogger(DMConversationController.class);
@@ -31,33 +36,38 @@ public class DMConversationController implements FxmlController {
     private final DirectMessages directMessages;
     private final Supplier<DirectMessageListCell> directMessageListCellSupplier;
 
-    private User pal = null;
-    private final ReadOnlyProperty<User> palProperty;
+    private final Property<User> palProperty;
+    private final ListProperty<DirectMessage> loadedMessages;
 
     public DMConversationController(
             final DirectMessages directMessages,
             final ApplicationContext applicationContext
     ) {
         this.directMessages = directMessages;
-        this.palProperty = new ReadOnlyObjectWrapper<>(pal);
         this.directMessageListCellSupplier = () -> applicationContext.getBean(DirectMessageListCell.class);
+        this.palProperty = new SimpleObjectProperty<>(null);
+        this.loadedMessages = new SimpleListProperty<>(FXCollections.observableList(Collections.emptyList()));
     }
 
     @Override
     public void initialize() {
-        LOG.debug("Created conversation view for conversing with user!");
         LOG.debug("Schedule displaying of conversation once senderId has been received!");
-        palProperty.addListener((palVal, oldVal, newVal) -> deferredPalSetCall(newVal));
         messageListView.setCellFactory(messages -> directMessageListCellSupplier.get());
+        palProperty.addListener((palVal, oldVal, newVal) -> {
+            LOG.debug("Detected palProperty being set! New value : {}", newVal.getScreenName());
+            loadConversationContents(newVal);
+            messageListView.itemsProperty().bind(loadedMessages);
+        });
     }
 
     public void setPal(User pal) {
+        LOG.debug("Created conversation view for conversing with user!");
         LOG.debug("Mapping DM conversation view {} with senderId {}", this, pal.getScreenName());
-        this.pal = pal;
+        this.palProperty.setValue(pal);
     }
 
-    private void deferredPalSetCall(final User palValue) {
-        this.messageListView.getItems().addAll(directMessages.loadedConversations().get(palValue));
+    private void loadConversationContents(final User user) {
+        this.loadedMessages.setValue(FXCollections.observableList(directMessages.loadedConversations().get(user)));
     }
 
 }
