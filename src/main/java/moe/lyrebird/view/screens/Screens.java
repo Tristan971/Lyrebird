ff3251a16d5b134e01825bb87fee8134c6440112
Tristan Deloche <tristandeloche@gmail.com>
2018-06-19 15:10:41 +0200

    Add credits view

--- src/main/java/moe/lyrebird/view/screens/Screens.java
+++ src/main/java/moe/lyrebird/view/screens/Screens.java
@@ -3,6 +3,7 @@ package moe.lyrebird.view.screens;
 import moe.tristan.easyfxml.api.FxmlController;
 import moe.tristan.easyfxml.api.FxmlFile;
 import moe.tristan.easyfxml.api.FxmlNode;
+import moe.lyrebird.view.screens.credits.CreditsController;
 import moe.lyrebird.view.screens.login.LoginViewController;
 import moe.lyrebird.view.screens.newtweet.NewTweetController;
 import moe.lyrebird.view.screens.root.RootViewController;
@@ -12,9 +13,11 @@ import moe.lyrebird.view.screens.root.RootViewController;
  */
 @SuppressWarnings("unused")
 public enum Screens implements FxmlNode {
+
     ROOT_VIEW("root/RootView.fxml", RootViewController.class),
     LOGIN_VIEW("login/Login.fxml", LoginViewController.class),
-    TWEET_VIEW("newtweet/NewTweet.fxml", NewTweetController.class);
+    TWEET_VIEW("newtweet/NewTweet.fxml", NewTweetController.class),
+    CREDITS_VIEW("credits/Credits.fxml", CreditsController.class);
 
     private static final String SCREENS_BASE_PATH = "moe/lyrebird/view/screens/";
 
---
+++ src/main/java/moe/lyrebird/view/screens/credits/CreditsController.java
@@ -0,0 +1,14 @@
+package moe.lyrebird.view.screens.credits;
+
+import org.springframework.stereotype.Component;
+import moe.tristan.easyfxml.api.FxmlController;
+
+@Component
+public class CreditsController implements FxmlController {
+
+    @Override
+    public void initialize() {
+
+    }
+
+}
