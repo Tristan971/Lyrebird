540f8acaa0f323169c26f36862df77166023ea78
Tristan Deloche <tristandeloche@gmail.com>
2018-06-19 16:06:07 +0200

    Fix resource loading

--- src/main/java/moe/lyrebird/view/screens/credits/CreditsController.java
+++ src/main/java/moe/lyrebird/view/screens/credits/CreditsController.java
@@ -1,36 +1,70 @@
 package moe.lyrebird.view.screens.credits;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 import moe.tristan.easyfxml.api.FxmlController;
-
-import org.assertj.core.util.Sets;
+import moe.tristan.easyfxml.model.exception.ExceptionHandler;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 
-import java.util.Set;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.TEXT_HTML_VALUE;
 
 @Component
 public class CreditsController implements FxmlController {
 
+    private final PathMatchingResourcePatternResolver pmrpr;
+
     @FXML
     private VBox creditsVBox;
+
+    public CreditsController() {
+        this.pmrpr = new PathMatchingResourcePatternResolver();
+    }
 
     @Override
     public void initialize() {
         findAllCreditsPages().stream()
-                        .map(html -> {
-                            final WebView webView = new WebView();
-                            webView.getEngine().loadContent(html, TEXT_HTML_VALUE);
-                            return webView;
-                        }).forEach(creditsVBox.getChildren()::add);
+                             .map(html -> {
+                                 final WebView webView = new WebView();
+                                 webView.getEngine().loadContent(html, TEXT_HTML_VALUE);
+                                 return webView;
+                             }).forEach(creditsVBox.getChildren()::add);
     }
 
-    public Set<String> findAllCreditsPages() {
-        return Sets.newTreeSet("TEST");
+    public List<String> findAllCreditsPages() {
+        try {
+            final Resource[] credits = pmrpr.getResources("classpath:assets/credits/*");
+            final List<String> loadedCredits = new ArrayList<>(credits.length);
+
+            for (final Resource credit: credits) {
+                final File creditResFile = credit.getFile();
+                final InputStream is = new FileInputStream(creditResFile);
+                final String content = StreamUtils.copyToString(is, StandardCharsets.UTF_8);
+                loadedCredits.add(content);
+            }
+
+            return loadedCredits;
+        } catch (final IOException e) {
+            ExceptionHandler.displayExceptionPane(
+                    "Error loading credits",
+                    "",
+                    e
+            );
+            return Collections.emptyList();
+        }
     }
 
 }
