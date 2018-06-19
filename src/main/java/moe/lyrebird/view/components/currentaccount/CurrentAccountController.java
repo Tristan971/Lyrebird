5eb7f8759cc82f906176ff9231b1be6ee6eda3d1
Tristan Deloche <tristandeloche@gmail.com>
2018-06-19 16:33:58 +0200

    Add preload current user profile picture

--- src/main/java/moe/lyrebird/view/components/currentaccount/CurrentAccountController.java
+++ src/main/java/moe/lyrebird/view/components/currentaccount/CurrentAccountController.java
@@ -3,17 +3,21 @@ package moe.lyrebird.view.components.currentaccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import moe.tristan.easyfxml.api.FxmlController;
+import io.vavr.control.Try;
 import moe.lyrebird.model.sessions.Session;
 import moe.lyrebird.model.sessions.SessionManager;
 import moe.lyrebird.view.CachedDataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import twitter4j.User;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 import java.util.concurrent.CompletableFuture;
+
+import static moe.lyrebird.view.components.ImageResources.BLANK_USER_PROFILE_PICTURE_LIGHT;
 
 @Component
 public class CurrentAccountController implements FxmlController {
@@ -40,6 +44,7 @@ public class CurrentAccountController implements FxmlController {
 
     @Override
     public void initialize() {
+        userProfilePicture.setImage(BLANK_USER_PROFILE_PICTURE_LIGHT.getImage());
         bindUsername();
         bindProfilePicture();
     }
@@ -56,15 +61,16 @@ public class CurrentAccountController implements FxmlController {
     }
 
     private void userChanged(final Session newValue) {
-        CompletableFuture.runAsync(
-                () -> newValue.getTwitterUser()
-                              .map(cachedDataService::userProfileImage)
-                              .onSuccess(userProfilePicture::setImage)
-                              .onFailure(err -> LOG.error(
-                                      "Error getting PP for current session!",
-                                      err
-                              ))
-        );
+        CompletableFuture.runAsync(() -> loadAndSetUserAvatar(newValue.getTwitterUser()));
+    }
+
+    private void loadAndSetUserAvatar(final Try<User> user) {
+        user.map(cachedDataService::userProfileImage)
+            .onSuccess(userProfilePicture::setImage)
+            .onFailure(err -> LOG.error(
+                    "Error getting PP for current session!",
+                    err
+            ));
     }
 
 }
