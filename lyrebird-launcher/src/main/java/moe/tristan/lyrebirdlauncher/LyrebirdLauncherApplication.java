0591f152e73c3c80c64b57645c1e6ff69fe8f4dc
Tristan971 <tristandeloche@gmail.com>
2018-06-29 23:18:11 +0200

    Add a launcher module and add a common parent

---
+++ lyrebird-launcher/src/main/java/moe/tristan/lyrebirdlauncher/LyrebirdLauncherApplication.java
@@ -0,0 +1,12 @@
+package moe.tristan.lyrebirdlauncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyrebirdLauncherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyrebirdLauncherApplication.class, args);
+    }
+}
