c5b610690994d141fa888e3deee472680d272d89
Tristan971 <tristandeloche@gmail.com>
2018-07-01 05:01:32 +0200

    Create module for server-side API

---
+++ lyrebird-server/src/main/java/moe/lyrebird/lyrebirdserver/LyrebirdServerApplication.java
@@ -0,0 +1,13 @@
+package moe.lyrebird.lyrebirdserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyrebirdServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyrebirdServerApplication.class, args);
+    }
+
+}
---
+++ lyrebird-server/src/test/java/moe/lyrebird/lyrebirdserver/LyrebirdServerApplicationTests.java
@@ -0,0 +1,16 @@
+package moe.lyrebird.lyrebirdserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LyrebirdServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
