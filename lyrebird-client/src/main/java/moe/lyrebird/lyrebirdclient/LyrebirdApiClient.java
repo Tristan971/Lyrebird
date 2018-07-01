507cfdb833f1a103b6e6b76cc233ac6d4ae8e84e
Tristan971 <tristandeloche@gmail.com>
2018-07-01 19:56:30 +0200

    Set-up basic client module

---
+++ lyrebird-client/src/main/java/moe/lyrebird/lyrebirdclient/LyrebirdApiClient.java
@@ -0,0 +1,13 @@
+package moe.lyrebird.lyrebirdclient;
+
+import org.springframework.stereotype.Service;
+import moe.lyrebird.lyrebirdserver.model.objects.LyrebirdVersion;
+
+@Service
+public class LyrebirdApiClient {
+
+    public LyrebirdVersion getLatestVersion() {
+
+    }
+
+}
--- lyrebird-client/src/test/java/moe/lyrebird/lyrebirdclient/LyrebirdClientApplicationTests.java
+++ lyrebird-client/src/test/java/moe/lyrebird/lyrebirdclient/LyrebirdClientApplicationTests.java
@@ -1,4 +1,4 @@
-package moe.tristan.lyrebirdlauncher;
+package moe.lyrebird.lyrebirdclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LyrebirdLauncherApplicationTests {
+public class LyrebirdClientApplicationTests {
 
     @Test
     public void contextLoads() {
