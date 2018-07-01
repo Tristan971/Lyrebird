15f9cb49f4036d75fdc898bb29934d57440ada3a
Tristan971 <tristandeloche@gmail.com>
2018-07-01 20:37:21 +0200

    Lots of broken stuff atm

---
+++ lyrebird-api-client/src/main/java/moe/lyrebird/api/client/LyrebirdServerClient.java
@@ -0,0 +1,31 @@
+package moe.lyrebird.api.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import moe.lyrebird.api.server.LyrebirdApiServer;
+
+@Component
+public class LyrebirdServerClient {
+
+    private static final String API_URL = "https://tristan.moe";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public LyrebirdServerClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public LyrebirdVersion getLatestVersion() {
+        return restTemplate.getForEntity(
+                buildUrl(Endpoints.VERSIONS_CONTROLLER, Endpoints.VERSIONS_LATEST),
+                LyrebirdVersion.class
+        ).getBody();
+    }
+
+    private String buildUrl(final String controller, final String method) {
+        return API_URL + "/" + controller + "/" + method;
+    }
+
+}
---
+++ lyrebird-api-client/src/test/java/moe/lyrebird/lyrebirdapiclient/LyrebirdApiClientApplicationTests.java
@@ -0,0 +1,16 @@
+package moe.lyrebird.lyrebirdapiclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LyrebirdApiClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
