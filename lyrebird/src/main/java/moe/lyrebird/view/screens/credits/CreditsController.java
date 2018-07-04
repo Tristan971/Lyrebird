25d23d99e35f9ad19a2c74f246dcda9c2254aeca
Tristan971 <tristandeloche@gmail.com>
2018-07-04 22:04:47 +0200

    Add copyright notice to all files.

--- lyrebird/src/main/java/moe/lyrebird/view/screens/credits/CreditsController.java
+++ lyrebird/src/main/java/moe/lyrebird/view/screens/credits/CreditsController.java
@@ -1,3 +1,21 @@
+/*
+ *     Lyrebird, a free open-source cross-platform twitter client.
+ *     Copyright (C) 2017-2018, Tristan Deloche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package moe.lyrebird.view.screens.credits;
 
 import org.springframework.core.io.Resource;
