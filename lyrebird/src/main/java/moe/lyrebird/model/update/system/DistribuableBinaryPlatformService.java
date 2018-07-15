b9d1601f23e2d507a208a34343ecba672208ebf8
Tristan971 <tristandeloche@gmail.com>
2018-07-16 01:44:52 +0200

    Add a platform mapping service for running binary.

---
+++ lyrebird/src/main/java/moe/lyrebird/model/update/system/DistribuableBinaryPlatformService.java
@@ -0,0 +1,54 @@
+/*
+ *     Lyrebird, a free open-source cross-platform twitter client.
+ *     Copyright (C) 2017-2018, Tristan Deloche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package moe.lyrebird.model.update.system;
+
+import org.springframework.stereotype.Component;
+import io.vavr.control.Option;
+import moe.lyrebird.api.server.model.objects.TargetPlatform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import oshi.SystemInfo;
+
+@Component
+public class DistribuableBinaryPlatformService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DistribuableBinaryPlatformService.class);
+
+    public Option<TargetPlatform> detectRunningPlatform() {
+        LOG.debug("Detecting platform...");
+        switch (SystemInfo.getCurrentPlatformEnum()) {
+            case WINDOWS:
+                LOG.debug("Running on Windows");
+                return Option.of(TargetPlatform.WINDOWS);
+            case LINUX:
+                LOG.debug("Running on Linux.");
+                return Option.none();
+            case MACOSX:
+                LOG.debug("Running on macOS.");
+                return Option.of(TargetPlatform.MACOS);
+            case SOLARIS:
+            case FREEBSD:
+            case UNKNOWN:
+            default:
+                LOG.debug("Unknown platform : {}", SystemInfo.getCurrentPlatformEnum());
+                return Option.none();
+        }
+    }
+
+}
