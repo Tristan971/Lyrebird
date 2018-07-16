aa86e1919993152c91fe488c5842a4fabca97e16
Tristan971 <tristandeloche@gmail.com>
2018-07-16 02:37:02 +0200

    Add very basic integration test for downloading the binary

---
+++ lyrebird/src/test/java/moe/lyrebird/model/update/system/DistribuableInstallationServiceTest.java
@@ -0,0 +1,55 @@
+/*
+ *     Lyrebird, a free open-source cross-platform twitter client.
+ *     Copyright (C) 2017-2018, Tristan Deloche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package moe.lyrebird.model.update.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import moe.lyrebird.api.client.LyrebirdServerClient;
+import moe.lyrebird.api.server.model.objects.LyrebirdPackage;
+import moe.lyrebird.api.server.model.objects.LyrebirdVersion;
+import moe.lyrebird.api.server.model.objects.TargetPlatform;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DistribuableInstallationServiceTest {
+
+    @Autowired
+    private LyrebirdServerClient lyrebirdServerClient;
+
+    @Autowired
+    private DistribuableInstallationService distribuableInstallationService;
+
+    @Test
+    public void getInstallationCommandLine() {
+        final LyrebirdVersion latestVersion = lyrebirdServerClient.getLatestVersion();
+
+        final List<LyrebirdPackage> packages = latestVersion.getPackages();
+
+        distribuableInstallationService.getInstallationCommandLine(
+                TargetPlatform.WINDOWS,
+                latestVersion
+        );
+    }
+
+}
