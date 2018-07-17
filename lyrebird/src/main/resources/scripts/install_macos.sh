094614cb795383629b5bde5d6472e1d45f120575
Tristan Deloche <tristandeloche@gmail.com>
2018-07-17 22:13:52 +0200

    Update macOS script with some output

--- lyrebird/src/main/resources/scripts/install_macos.sh
+++ lyrebird/src/main/resources/scripts/install_macos.sh
@@ -24,25 +24,31 @@
 #
 
 DMG_FILE="$1"
-APP_PID="$2"
-TMP_NAME="lyrebird$(date +%s)"
+TMP_NAME="lyrebird-update-$(date +%s)"
 
-while /bin/ps -p ${APP_PID} > /dev/null; do
-  /bin/sleep 1;
-done
+echo "Lyrebird macOS update script starting!"
+echo "Will update to version in DMG file : ${DMG_FILE}"
 
+echo "Converting ${DMG_FILE} file to CDR/UDTO at position : /tmp/${TMP_NAME}.cdr"
 /usr/bin/hdiutil convert -quiet "$DMG_FILE" -format UDTO -o /tmp/${TMP_NAME}
+
+echo "Attaching /tmp/${TMP_NAME}"
 /usr/bin/hdiutil attach -quiet -nobrowse -noautoopen -readonly -mountpoint /Volumes/${TMP_NAME} /tmp/${TMP_NAME}.cdr
 
 shopt -s nullglob
 
 pushd /Volumes/${TMP_NAME} > /dev/null
 for app in *.app; do
+  echo "Deleting /Applications/$app"
   /bin/rm -rf "/Applications/$app"
+  echo "Copying $app to /Applications"
   /bin/cp -pR "/Volumes/$TMP_NAME/$app" /Applications/
 done
 popd > /dev/null
 
+echo "Detaching disk image..."
 /usr/bin/hdiutil detach -quiet /Volumes/${TMP_NAME}
+
+echo "Deleting /tmp/${TMP_NAME}.cdr"
 /bin/rm /tmp/${TMP_NAME}.cdr
 /bin/rm "$DMG_FILE"
