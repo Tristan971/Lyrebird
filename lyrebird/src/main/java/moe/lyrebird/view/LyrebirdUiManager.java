3fc91c865416bc733d7326c7ac724761f1d6094b
Tristan971 <tristandeloche@gmail.com>
2018-07-20 21:40:50 +0200

    Paramterize app name

--- lyrebird/src/main/java/moe/lyrebird/view/LyrebirdUiManager.java
+++ lyrebird/src/main/java/moe/lyrebird/view/LyrebirdUiManager.java
@@ -53,7 +53,7 @@ public class LyrebirdUiManager extends FxUiManager {
     protected String title() {
         return String.format(
                 "%s [%s]",
-                "Lyrebird",
+                environment.getProperty("app.name"),
                 environment.getProperty("app.version")
         );
     }
