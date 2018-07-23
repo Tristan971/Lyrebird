9c39ae47c3dc742b285bb0c8886d1ac85d75aebc
Tristan971 <tristandeloche@gmail.com>
2018-07-23 06:12:26 +0200

    Add support for user preferences persistence

---
+++ lyrebird/src/main/java/moe/lyrebird/model/settings/SettingsService.java
@@ -0,0 +1,43 @@
+package moe.lyrebird.model.settings;
+
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.prefs.Preferences;
+
+/**
+ * This class manages persistent user application settings.
+ *
+ * It is mostly a verbose decorator around {@link Preferences#userRoot()}.
+ */
+@Component
+public class SettingsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SettingsService.class);
+
+    private static final Preferences USER_PREFERENCES = Preferences.userRoot();
+
+    public String get(final Setting setting, final String defaultValue) {
+        final String value = USER_PREFERENCES.get(setting.getPreferenceKey(), defaultValue);
+        LOG.debug("Fetched user setting {} : {}", setting, value);
+        return value;
+    }
+
+    public void set(final Setting setting, final String value) {
+        LOG.debug("Saving user setting {} with value {}", setting, value);
+        USER_PREFERENCES.put(setting.getPreferenceKey(), value);
+    }
+
+    public boolean get(final Setting setting, final boolean defaultValue) {
+        final boolean value = USER_PREFERENCES.getBoolean(setting.getPreferenceKey(), defaultValue);
+        LOG.debug("Fetched boolean user setting {} : {}", setting, value);
+        return value;
+    }
+
+    public void set(final Setting setting, final boolean value) {
+        LOG.debug("Saving boolean user setting {} with value {}", setting, value);
+        USER_PREFERENCES.putBoolean(setting.getPreferenceKey(), value);
+    }
+
+}
