7af79ca7c44716a4c68619a2846731f52851a0ba
Tristan971 <tristandeloche@gmail.com>
2018-08-14 21:06:44 +0100

    Add small utility class for basic async prop setting management

---
+++ lyrebird/src/main/java/moe/lyrebird/model/util/FXProperties.java
@@ -0,0 +1,30 @@
+package moe.lyrebird.model.util;
+
+import java.util.function.Consumer;
+
+import javafx.beans.value.ObservableValue;
+
+public final class FXProperties {
+
+    private FXProperties() {
+    }
+
+    public static <T> void waitForProp(final ObservableValue<T> prop, final Consumer<T> onReady) {
+        if (prop.getValue() == null) {
+            prop.addListener((observable, oldValue, newValue) -> {
+                onReady.accept(newValue);
+            });
+        } else {
+            onReady.accept(prop.getValue());
+        }
+    }
+
+    public static void waitForBooleanProp(final ObservableValue<Boolean> prop, final Runnable onReady) {
+        if (prop.getValue() != null && prop.getValue()) {
+            onReady.run();
+        } else {
+            waitForProp(prop, val -> onReady.run());
+        }
+    }
+
+}
