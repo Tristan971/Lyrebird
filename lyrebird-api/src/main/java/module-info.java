11927f356de9bb93592173e2da2a19bbb8a48793
Tristan971 <tristandeloche@gmail.com>
2018-08-14 12:45:17 +0100

    Modularize API module

---
+++ lyrebird-api/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module moe.lyrebird.api {
+
+    requires slf4j.api;
+
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.web;
+
+    requires com.fasterxml.jackson.databind;
+    requires jackson.annotations;
+
+}
