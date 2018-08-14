513ec312b8ba0aa4e839c439752cb4606b8d9e59
Tristan971 <tristandeloche@gmail.com>
2018-08-14 13:59:05 +0100

    Basic modularization of client app

---
+++ lyrebird/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module lyrebird {
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires easyfxml;
+    requires SystemTray;
+    requires java.desktop;
+    requires moe.lyrebird.api;
+    requires javafx.graphics;
+    requires slf4j.api;
+    requires spring.beans;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires twitter4a.core;
+    requires io.vavr;
+    requires spring.core;
+    requires javafx.web;
+    requires javafx.media;
+    requires java.prefs;
+    requires java.persistence;
+    requires com.fasterxml.jackson.databind;
+    requires jackson.annotations;
+
+}
