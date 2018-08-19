f9c6965e07841275e88dd1b3da183c407b527c6e
Tristan971 <tristandeloche@gmail.com>
2018-08-19 21:19:14 +0100

    Reformat packagekit service for now until we make it work

--- lyrebird/src/main/java/moe/lyrebird/model/update/selfupdate/linux/PackageKitService.java
+++ lyrebird/src/main/java/moe/lyrebird/model/update/selfupdate/linux/PackageKitService.java
@@ -1,42 +1,24 @@
 package moe.lyrebird.model.update.selfupdate.linux;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-import org.freedesktop.dbus.CallbackHandler;
 import org.freedesktop.dbus.DBusConnection;
 import org.freedesktop.dbus.DBusInterface;
 import org.freedesktop.dbus.DBusInterfaceName;
 import org.freedesktop.dbus.UInt32;
 import org.freedesktop.dbus.exceptions.DBusException;
-import org.freedesktop.dbus.exceptions.DBusExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
-public class PackageKitService {
+public final class PackageKitService {
 
     private static final Logger LOG = LoggerFactory.getLogger(PackageKitService.class);
 
-    private AtomicReference<DBusConnection> dbusConnectionHolder = new AtomicReference<>();
-
-    private DBusConnection getDbusConnectionHolder() {
-        if (dbusConnectionHolder.getAcquire() == null) {
-            try {
-                dbusConnectionHolder.setRelease(DBusConnection.getConnection(DBusConnection.SESSION));
-            } catch (DBusException e) {
-                LOG.error("Could not acquire connection to the DBus messaging system!", e);
-            }
-        }
-        return dbusConnectionHolder.get();
-    }
-
-    public void showNativeUnixPackageKitPrompt() {
+    public static void showNativeUnixPackageKitPrompt() {
         LOG.info("Calling native unix update prompt provided by PackageKit over DBus");
 
-        final DBusConnection conn = getDbusConnectionHolder();
-        LOG.debug("Got DBus connection : {}", conn);
         try {
+            final DBusConnection conn = DBusConnection.getConnection(DBusConnection.SESSION);
+            LOG.debug("Got DBus connection : {}", conn);
+
             final IPackageKit packageKit = conn.getRemoteObject(
                     "org.freedesktop.PackageKit",
                     "/org/freedesktop/PackageKit",
@@ -48,7 +30,7 @@ public class PackageKitService {
             final long displayNumber = displayName == null ? 0L : Integer.parseInt(displayName.substring(1));
             LOG.debug("Will display update prompt on screen {} [from $DISPLAY = {}]", displayNumber, displayName);
 
-            packageKit.InstallPackageNames(new UInt32(displayNumber), new String[] {"lyrebird"}, "");
+            packageKit.InstallPackageNames(new UInt32(displayNumber), new String[] {"lyrebird"}, "show-confirm-search,hide-finished");
             LOG.debug("Successfully invoked PackageKit InstallPackageNames method.");
         } catch (DBusException e) {
             LOG.error("Could not invoke PackageKit", e);
@@ -56,19 +38,19 @@ public class PackageKitService {
 
     }
 
-}
+    /**
+     * This class represents a subset of the PackageKit Session DBus interface that's relevant to us.
+     *
+     * <a href="https://blog.fpmurphy.com/2013/11/packagekit-d-bus-abstraction-layer.html">PackageKit DBus abstraction layer</a>
+     * <a href="https://techbase.kde.org/Development/Tutorials/PackageKit_Session_Interface">PackageKit Session Interface</a>
+     *
+     * @author Lourkeur
+     */
+    @DBusInterfaceName("org.freedesktop.PackageKit.Transaction")
+    interface IPackageKit extends DBusInterface {
 
-/**
- * This class represents a subset of the PackageKit Session DBus interface that's relevant to us.
- *
- * <a href="https://blog.fpmurphy.com/2013/11/packagekit-d-bus-abstraction-layer.html">PackageKit DBus abstraction layer</a>
- * <a href="https://techbase.kde.org/Development/Tutorials/PackageKit_Session_Interface">PackageKit Session Interface</a>
- *
- * @author Lourkeur
- */
-@DBusInterfaceName("org.freedesktop.PackageKit.Modify")
-interface IPackageKit extends DBusInterface {
+        void InstallPackageNames(UInt32 xid, String[] packages, String interaction);
 
-    void InstallPackageNames(UInt32 xid, String[] packages, String interaction);
+    }
 
 }
---
+++ lyrebird/src/test/java/moe/lyrebird/model/update/selfupdate/linux/PackageKitServiceTest.java
@@ -0,0 +1,12 @@
+package moe.lyrebird.model.update.selfupdate.linux;
+
+import org.junit.Test;
+
+public class PackageKitServiceTest {
+
+    @Test
+    public void showNativeUnixPackageKitPrompt() {
+        PackageKitService.showNativeUnixPackageKitPrompt();
+    }
+
+}
